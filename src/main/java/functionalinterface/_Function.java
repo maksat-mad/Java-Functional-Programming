a821639753d2be4523da96b71ec716b32a20cd8e
maksat-madeniyetov <maksat.madeniyetov@gmail.com>
2022-05-04 21:03:03 +0600

    Function, apply(), andThen()

---
+++ src/main/java/functionalinterface/_Function.java
@@ -0,0 +1,30 @@
+package functionalinterface;
+
+import java.util.function.Function;
+
+public class _Function {
+    public static void main(String[] args) {
+        int increment = incrementByOne(1);
+        System.out.println(increment);
+
+        Integer increment2 = incrementByOneFunction.apply(1);
+        System.out.println(increment2);
+
+        int multiply = multiplyBy10Function.apply(increment2);
+        System.out.println(multiply);
+
+        Function<Integer, Integer> addBy1AndThenMultiplyBy10 =
+                incrementByOneFunction.andThen(multiplyBy10Function);
+        System.out.println(addBy1AndThenMultiplyBy10.apply(4));
+    }
+
+    static Function<Integer, Integer> incrementByOneFunction =
+            number -> number + 1;
+
+    static Function<Integer, Integer> multiplyBy10Function =
+            number -> number * 10;
+
+    static int incrementByOne(int number) {
+        return number + 1;
+    }
+}
